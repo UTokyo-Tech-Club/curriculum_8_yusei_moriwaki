3b489e6b5f302e90f218fae33ed36421677f985b
Yusei Moriwaki <yusei.mo@icloud.com>
2025-09-17 10:16:19 +0900

    Add state management to App component
    - Introduced useState hook to manage a count state.
    - Updated button functionality to increment the count on click, displaying the current count in the UI.

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={() => console.log('push')}>Button</button>
+        {count}
+        <button onClick={() => setCount(count + 1)}>Button</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
